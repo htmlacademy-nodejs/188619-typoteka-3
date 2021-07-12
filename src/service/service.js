36123c515d429c14a8e91e74971be4e1037b7b92
Bulat <garifb@gmail.com>
2021-07-12 23:14:20 +0300

    feat(service): cli help service

--- src/service/service.js
+++ src/service/service.js
@@ -1,3 +1,17 @@
 'use strict';
 
-console.log(`Hello, world!`);
+const {Cli} = require(`./cli`);
+
+const USER_ARGV_INDEX = 2;
+const DEFAULT_COMMAND = `--help`;
+const SUCCESS_EXIT_CODE = 0;
+
+const userArguments = process.argv.slice(USER_ARGV_INDEX);
+const [userCommand] = userArguments;
+
+if (userArguments.length === 0 || !Cli[userCommand]) {
+  Cli[DEFAULT_COMMAND].run();
+  process.exit(SUCCESS_EXIT_CODE);
+}
+
+Cli[userCommand].run(userArguments.slice(1));
