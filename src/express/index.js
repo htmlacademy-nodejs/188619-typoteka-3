600a17c4ca65b69b684bfef339b4f1489bdec0be
Bulat <garifb@gmail.com>
2021-07-23 16:37:06 +0300

    feat(express): pug templates

--- src/express/index.js
+++ src/express/index.js
@@ -1,14 +1,20 @@
 'use strict';
 
+const path = require(`path`);
 const express = require(`express`);
 const mainRoutes = require(`./routes/main-routes`);
 const articlesRoutes = require(`./routes/article-routes`);
 const myRoutes = require(`./routes/my-routes`);
 
+const PUBLIC_DIR = `public`;
 const DEFAULT_PORT = 8080;
 
 const app = express();
 
+app.set(`views`, path.resolve(__dirname, `templates`));
+app.set(`view engine`, `pug`);
+
+app.use(express.static(path.resolve(__dirname, PUBLIC_DIR)));
 app.use(`/`, mainRoutes);
 app.use(`/articles`, articlesRoutes);
 app.use(`/my`, myRoutes);
