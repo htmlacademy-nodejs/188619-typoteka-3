642c1cd1cbe07d347c61c7f77604a5349ccaaa8c
Bulat <garifb@gmail.com>
2021-07-24 19:58:49 +0300

    feat(cli): server command handled by express

--- src/service/cli/server.js
+++ src/service/cli/server.js
@@ -1,69 +1,40 @@
 'use strict';
 
-const http = require(`http`);
+const express = require(`express`);
 const fs = require(`fs`).promises;
 const chalk = require(`chalk`);
+const {HttpCode} = require(`../../constants`);
 
-const {
-  HttpCode,
-  MOCKS_FILE_NAME
-} = require(`../../constants`);
 const DEFAULT_PORT = 3000;
+const FILENAME = `mocks.json`;
 
-const sendResponse = (res, statusCode, message) => {
-  const template = `
-    <!Doctype html>
-      <html lang="ru">
-      <head>
-        <title>With love from Node</title>
-      </head>
-      <body>${message}</body>
-    </html>`.trim();
-
-  res.writeHead(statusCode, {
-    'Content-Type': `text/html; charset=UTF-8`,
-  });
-
-  res.end(template);
-};
-
-const onClientConnect = async (req, res) => {
-  const notFoundMessageText = `Not found`;
-
-  switch (req.url) {
-    case `/`:
-      try {
-        const fileContent = await fs.readFile(MOCKS_FILE_NAME);
-        const mocks = JSON.parse(fileContent);
-        const message = mocks.map((post) => `<li>${post.title}</li>`).join(``);
-        sendResponse(res, HttpCode.OK, `<ul>${message}</ul>`);
-      } catch (err) {
-        sendResponse(res, HttpCode.NOT_FOUND, notFoundMessageText);
-      }
-
-      break;
-    default:
-      sendResponse(res, HttpCode.NOT_FOUND, notFoundMessageText);
-      break;
-  }
-};
-
-const createServer = (port) => {
-  http.createServer(onClientConnect)
-    .listen(port)
-    .on(`listening`, () => {
-      console.info(chalk.green(`Ожидаю соединений на ${port}`));
-    })
-    .on(`error`, ({message}) => {
-      console.error(chalk.red(`Ошибка при создании сервера: ${message}`));
-    });
-};
+const app = express();
+app.use(express.json());
 
 module.exports = {
   name: `--server`,
   run(args) {
     const [customPort] = args;
     const port = Number.parseInt(customPort, 10) || DEFAULT_PORT;
-    createServer(port);
+
+    app.get(`/posts`, async (req, res) => {
+      try {
+        const fileContent = await fs.readFile(FILENAME);
+        const mocks = JSON.parse(fileContent);
+        res.json(mocks);
+      } catch (err) {
+        res.status(HttpCode.INTERNAL_SERVER_ERROR).send(err);
+      }
+    });
+
+    app.use((req, res) => res
+      .status(HttpCode.NOT_FOUND)
+      .send(`Not found`));
+
+    app.listen(port, () => {
+      console.info(chalk.green(`Ожидаю соединений на ${port}`));
+    }).on(`error`, (err) => {
+      console.error(chalk.red(`Ошибка при создании сервера: ${err}`));
+    });
   }
 };
