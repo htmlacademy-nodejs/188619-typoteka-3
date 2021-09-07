cc3fb711ddab5e4d3a12692d21de48b09064c106
Bulat <garifb@gmail.com>
2021-09-07 21:37:49 +0300

    feat: create logs dir before start

--- src/service/lib/logger.js
+++ src/service/lib/logger.js
@@ -1,17 +1,28 @@
 "use strict";
 
+const fs = require(`fs`);
+const path = require(`path`);
 const pino = require(`pino`);
 const {Env} = require(`../../constants`);
 
-const LOG_FILE = `./logs/api.log`;
+const LOG_DIR = `./logs`;
+const LOG_FILE = `api.log`;
+
 const isDevMode = process.env.NODE_ENV !== Env.PRODUCTION;
 const defaultLogLevel = isDevMode ? `info` : `error`;
+
+const getLogDir = () => {
+  if (!fs.existsSync(LOG_DIR)) {
+    fs.mkdirSync(LOG_DIR);
+  }
+  return path.join(LOG_DIR, LOG_FILE);
+};
 
 const logger = pino({
   name: `base-logger`,
   level: process.env.LOG_LEVEL || defaultLogLevel,
   prettyPrint: isDevMode
-}, isDevMode ? process.stdout : pino.destination(LOG_FILE));
+}, isDevMode ? process.stdout : pino.destination(getLogDir()));
 
 module.exports = {
   logger,
